d87d061147a5f81baf6ca598572dbe0815711986
NTsoi <czoj.n95@mail.ru>
2021-10-08 13:03:34 +0300

    adds v1

--- HW1-VaraintsMerge/src/Variants.java
+++ HW1-VaraintsMerge/src/Variants.java
@@ -4,7 +4,7 @@ public class Variants {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("There are following variants:");
-
+		System.out.println("Variant 1");
 	}
 
 }
